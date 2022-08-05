777944c4f44abd75fd9bfe6efa4d69e660f29df7
ramlovewanshi <ramsoftware786@gmail.com>
2022-08-05 10:27:16 +0530

    Update Fact.java

--- Fact.java
+++ Fact.java
@@ -1,11 +1,12 @@
+//Print Table of the Given Number
 import java.util.Scanner;
-class Fact{
+class Table{
 	public static void main(String args[]){
 	Scanner k=new Scanner(System.in);
 	int n,i;
 	System.out.println("Enter Any Number : ");
-	n=k.nextInt();  //4
-	for(i=1;i<=10;i++){  // 1 2 3 4 5 6 7 8 9 10
+	n=k.nextInt();  
+	for(i=1;i<=10;i++){  
 	System.out.println(""+n*i);
 	
 	}
